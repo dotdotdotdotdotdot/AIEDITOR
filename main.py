980f29205b9124ccbc192d64832d9bdde6d132f6
dotdotdotdotdotdot <olinwilliam7@gmail.com>
2023-12-16 01:45:48 +0100

    wow text

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from moviepy.video.fx.all import crop
 from pytube import YouTube
 from PIL import Image
 import cv2
-import sys
 
 class VideoCreator:
     def create_tiktok(self, main_directory, attention_directory, output_folder, rest_folder):
@@ -30,49 +29,26 @@ class VideoCreator:
                     start_time = i * segment_duration
                     end_time = min((i + 1) * segment_duration, main_clip.duration)
 
-                    # ------- Facecam shits --------
-                    # Load the cascade
-                    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+                    title_text = TextClip("Part " + str(i+1), font='Impact',color='black',fontsize=60, bg_color='white')
+                    title_text = title_text.set_position(('center', 0.85), relative=True).set_duration(10)
 
                     facecam_width = 250
                     facecam_height = 125
                     facecam_scale = 3
 
-                    frame = main_clip.get_frame(10) # Gets a frame from main vid
-                    fromarray = Image.fromarray(frame) # Gör framen till en "riktig bild"
-                    fromarray = fromarray.save("frame.png") # Sparar bilden (skitwack att man måste göra det men det funkar inte annars)
-
-                    # read and convert image
-                    img = cv2.imread('./frame.png')
-                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-                    # Detect faces
-                    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-                    # Sparar mittpunkten för ansiktet
-                    for (x, y, w, h) in faces:
-                        midpoint_y = y+(h/2)
-                        midpoint_x = x+(w/2)
-
-                    facecam = main_clip.subclip(start_time, end_time).crop(x_center=midpoint_x, y_center=midpoint_y, width=facecam_width, height=facecam_height) # croppar ut ansiktet
-                    facecam_scaled = facecam.resize(facecam_scale) #gör den större
-                    facecam_scaled = facecam_scaled.set_position(('center',0), relative=True)
-
-                    # ----- NO MORE FACE ------ (kanske stoppa i egen funktion elle nåt)
-
-                    
-
+                    facecam_scaled = self.get_facecam(facecam_width, facecam_height, facecam_scale, start_time, end_time, main_clip)
                     segment_main_clip = main_clip.subclip(start_time, end_time).resize(newsize=(main_width*aspect_ratio,(2*(main_height/3)))) # ändrar heighten till 2/3 av hela
-                    segment_main_clip_face = CompositeVideoClip([segment_main_clip, facecam_scaled])
+                    segment_main_clip_face = CompositeVideoClip([segment_main_clip, facecam_scaled, title_text])
                     segment_main_clip_cropped = crop(segment_main_clip_face, x1=((main_width*aspect_ratio-main_width)/2), width=main_width)
 
                     # Segment the attention video
-                    segment_attention_clip = attention_clip.subclip(0, min(segment_duration, attention_clip.duration)).resize(newsize=(main_width,(main_height/3))) # ändrar heighten till 1/3 av hela
+                    segment_attention_clip = attention_clip.subclip(start_time, end_time).resize(newsize=(main_width,(main_height/3))) # ändrar heighten till 1/3 av hela
 
                     # Composite the videos together
                     final_clip = clips_array([[segment_main_clip_cropped], [segment_attention_clip]])
                     output_filename = f"{self.next_file_number(output_folder)}.mp4"
                     output_path = os.path.join(output_folder, output_filename)
-                    final_clip.write_videofile(output_path, codec="libx264", fps=24)
+                    final_clip.write_videofile(output_path, codec="libx265", fps=24)
 
             # Move remaining parts of the main clip to the rest folder
             if main_clip.duration % segment_duration != 0:
@@ -96,8 +72,41 @@ class VideoCreator:
     
     def get_all_videos(self, path):
         return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.mp4')]
+    
+    def get_facecam(self, width, height, scale, start_time, end_time, clip):
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+        frame = clip.get_frame(10) # Gets a frame from main vid
+        fromarray = Image.fromarray(frame) # Gör framen till en "riktig bild"
+        fromarray = fromarray.save("frame.png") # Sparar bilden (skitwack att man måste göra det men det funkar inte annars)
+
+        # read and convert image
+        img = cv2.imread('./frame.png')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        # Sparar mittpunkten för ansiktet
+        for (x, y, w, h) in faces:
+            midpoint_y = y+(h/2)
+            midpoint_x = x+(w/2)
+        facecam = clip.subclip(start_time, end_time).crop(x_center=midpoint_x, y_center=midpoint_y, width=width, height=height) # croppar ut ansiktet
+        facecam_scaled = facecam.resize(scale) #gör den större
+        facecam_scaled = facecam_scaled.set_position(('center',0), relative=True)
+        return facecam_scaled
 
 class VideoDownloader():
+    def download_from_file(self, file_path, main_folder, ret_folder):
+        with open(file_path, 'r') as file:
+            for line in file:
+                if line.startswith('Main: '):
+                    url = line.replace('Main: ', '').strip()
+                    self.download_video(url, main_folder)
+                elif line.startswith('Ret: '):
+                    url = line.replace('Ret: ', '').strip()
+                    self.download_video(url, ret_folder)
+
     def download_video(self, url, path):
         name = f"{self.next_file_number(path)}.mp4"
         yt = YouTube(url)
@@ -114,29 +123,37 @@ class VideoUploader():
     def uploaded():
         NotImplementedError()
 
-class TxTWriter():
-    def write():
+class TxT():
+    def writer(self):
+        with open('videos.txt', 'w') as file:
+            link = input("paste link:")
+            categorized_link = self.categorize(link)
+            file.write(categorized_link + '\n')
 
-        
+    def read(self):
+        with open('video.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                print(line.strip())
 
+    def categorize(self, link):
+        choice = input("main or ret link?")
+        if choice not in ["main", "ret"]:
+            print("no")
+            return
+        elif choice == "main":
+            return "Main: " + link
+        elif choice == "ret":
+            return "Ret: " + link
+    
 
-        file = open('videos.txt' 'r+')
-
-
-
-        file.close()
-    def read():
-        NotImplemented
-
-        
 def main():
     creator = VideoCreator()
     downloader = VideoDownloader()
     uploader = VideoUploader()
-    txt = TxTWriter()
+    txt = TxT()
 
-    main_video_url = "https://www.youtube.com/watch?v=6RbvvsrMlFw"
-    retentioner_video_url = "https://www.youtube.com/watch?v=TW-LgJUiMX0"
+    videos = './videos.txt'
     main_videos_path = "./downloads/main"
     retention_videos_path = "./downloads/retention"
     edited_path = "./edited"
@@ -147,16 +164,18 @@ def main():
     menu = menu_file.read()
 
     def AddtoTxt():
-        NotImplemented
+        txt.read()
     
     def DownloadfromTxt():
-        downloader.download_video(main_video_url, main_videos_path)
-        downloader.download_video(retentioner_video_url, retention_videos_path)
+        downloader.download_from_file(videos, main_videos_path, retention_videos_path)
 
     def EditfromDownloads():
         creator.create_tiktok(main_videos_path, retention_videos_path, edited_path, rest_folder)
 
     def UploadfromEdited():
+        NotImplemented
+
+    def AutoMod():
         NotImplemented
 
     loop = True
@@ -169,7 +188,7 @@ def main():
         if choice == '1':
             AddtoTxt()
             loop = False
-        elif choice == '2':
+        if choice == '2':
             DownloadfromTxt()
             loop = False
         elif choice == '3':
@@ -177,6 +196,9 @@ def main():
             loop = False
         elif choice == '4':
             UploadfromEdited()
+            loop = False
+        elif choice == '5':
+            AutoMod()
             loop = False
 
 if __name__  == "__main__":
